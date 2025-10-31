7e6296c5911ea0e9252d543a0f0d83a76cc257ea
Truongminhdi96 <truongvy689@gmail.com>
2025-10-31 22:04:49 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Route mặc định
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
+
+// Import router
+const contactsRouter = require("./app/routes/contact.route");
+
+// Sử dụng router cho đường dẫn /api/contacts
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
